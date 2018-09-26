b3b591fa032b66acb81037af942f3fcd49ce0391
Ava Barron <mztriz@gmail.com>
2018-09-26 15:56:50 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(colors)
-  colors.each { |color| puts "#{color.inital.upcase}: #{color}" }
+  colors.each { |color| puts "#{color.initial.upcase}: #{color}" }
 end
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 display_rainbow(colors)
