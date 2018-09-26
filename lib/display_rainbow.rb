6d1728f6df2525d315012803efff6eb93909b920
Ava Barron <mztriz@gmail.com>
2018-09-26 15:53:42 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(colors)
   # put the first letter of the array to upcase then the element until the end of the array
-  colors.each { |color| puts "#{color.inital.upcase}: color}
+  colors.each { |color| puts "#{color.inital.upcase}: #{color}" }
 end
