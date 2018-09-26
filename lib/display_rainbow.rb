60b0e384be612286c97ba6640c5a30d5a392b95f
Ava Barron <mztriz@gmail.com>
2018-09-26 16:25:48 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -12,5 +12,6 @@ end
 =end
 
 def display_rainbow(colors)
+  #this was a pain to type 
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
