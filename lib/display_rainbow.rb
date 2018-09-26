5a6764c37279dc1dfa138ab573be512e7c2db23e
Ava Barron <mztriz@gmail.com>
2018-09-26 16:06:12 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 def display_rainbow(colors)
   colors.each do |color|
     if color.equal? colors.last
-      print "#{color[0,1].upcase}: #{color}"
+      print "#{color[0,1].upcase}: #{color}\n"
     else
       print "#{color[0,1].upcase}: #{color}, "
     end
